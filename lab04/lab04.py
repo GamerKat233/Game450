d361364147769e68c61223a48dfaa409ed3a92c9
GamerKat233 <91293027+GamerKat233@users.noreply.github.com>
2025-02-09 23:56:57 -0500

    Trade chatbot complete

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -20,8 +20,15 @@ def run_console_chat(sign, **kwargs):
                 print('Ending match:', ending_match)
             break
 
-lab04_params = {}
+lab04_params = {
+    "template_file":'lab04/lab04_trader_chat.json',
+    "inventory":'Mana potion, health potion, bread, eyeballs',
+    "sign":'Katy Richardson',
+    "end_regex":r"(.*)TRADE COMPLETE"
+}
 
 if __name__ ==  '__main__':
     # run lab04.py to test your template interactively
-    pass
+    trader_template_file = 'lab04/lab04_trader_chat.json'
+    run_console_chat(**lab04_params)
+    
